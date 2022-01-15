fc9bf27eb9aa4d1584a51a628cb9e31c70687467
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2022-01-15 15:03:27 +0100

    palettegroup: new class

---
+++ palettegroup.cpp
@@ -0,0 +1,81 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstring>
+
+#include "palettegroup.h"
+
+PaletteGroup::PaletteGroup(Palette *items[], unsigned numOfPalettes) : palettesNum(numOfPalettes)
+{
+    group = new Palette *[palettesNum];
+
+    memcpy(group, items, palettesNum * sizeof(Palette *));
+}
+
+PaletteGroup::~PaletteGroup()
+{
+    delete[] group;
+}
+
+Palette *PaletteGroup::getPalette(unsigned index)
+{
+    if (index < palettesNum)
+        return group[index];
+
+    return nullptr;
+}
+
+bool PaletteGroup::setPalette(unsigned index, Palette *palette)
+{
+    if (palette && (index < palettesNum))
+    {
+        group[index] = palette;
+        return true;
+    }
+
+    return false;
+}
+
+bool PaletteGroup::loadPalette(Palette *items[])
+{
+    if (items == nullptr)
+        return false;
+
+    for (unsigned i = 0; i < palettesNum; i++)
+    {
+        if (items[i] == nullptr)
+            return false;
+    }
+
+    if (group)
+        delete[] group;
+
+    group = new Palette *[palettesNum];
+
+    memcpy(group, items, palettesNum * sizeof(Palette *));
+    return true;
+}
+
+bool PaletteGroup::storePalette(Palette *items[])
+{
+    if (items == nullptr)
+        return false;
+    memcpy(items, group, palettesNum * sizeof(Palette *));
+    return true;
+}
---
+++ palettegroup.h
@@ -0,0 +1,59 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PALETTEGROUP_H_
+#define _PALETTEGROUP_H_
+
+#include "palette.h"
+
+class PaletteGroup
+{
+public:
+    enum
+    {
+        PAL_FRAME,
+        PAL_TITLEBAR,
+        PAL_PROGRESSBAR,
+        PAL_BACKGROUND,
+        PAL_ICONS,
+        PAL_MENU,
+        PAL_POPUP,
+        PAL_BUTTON,
+        PAL_SCROLLER,
+        PAL_LIST,
+        PAL_DESKTOP,
+        PAL_WINICON,
+        PAL_NUM
+    };
+
+    PaletteGroup() : group(nullptr), palettesNum(0) {}
+    explicit PaletteGroup(Palette *items[], unsigned numOfPalettes);
+    virtual ~PaletteGroup();
+
+    Palette *getPalette(unsigned index);
+    bool setPalette(unsigned index, Palette *palette);
+    bool loadPalette(Palette *items[]);
+    bool storePalette(Palette *items[]);
+
+private:
+    Palette **group;
+    unsigned palettesNum;
+};
+
+#endif
