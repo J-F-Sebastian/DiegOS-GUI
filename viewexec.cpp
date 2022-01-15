240ef0712e840c1ba7b1e5f166808beedb7f744c
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2022-01-15 15:18:56 +0100

    viewexec: new class

---
+++ viewexec.cpp
@@ -0,0 +1,21 @@
+#include "viewexec.h"
+#include "background.h"
+
+ViewExec::ViewExec(Rectangle &limits, ViewRender *rnd, PaletteGroup *pals, View *parent) : ViewGroup(limits, rnd, pals, parent)
+{
+    clearOptions(VIEW_OPT_ALL);
+}
+
+void ViewExec::initDesktop()
+{
+    Rectangle rect;
+    getLimits(rect);
+    View *bg = new Background(rect);
+    bg->clearResizeMode(VIEW_RESIZEABLE);
+    bg->setPalette(palettes->getPalette(PaletteGroup::PAL_DESKTOP));
+    insert(bg);
+}
+
+void ViewExec::initMenu()
+{
+}
