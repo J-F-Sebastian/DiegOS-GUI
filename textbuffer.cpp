82c0120e60b9a018ea7af8c7bcd0959b270ae8fa
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2022-01-15 15:19:09 +0100

    textbuffer: new class

---
+++ textbuffer.cpp
@@ -0,0 +1,75 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "textbuffer.h"
+
+TextBuffer::TextBuffer(int xres, int yres) : xres(xres), yres(yres)
+{
+    bufferSize = xres * yres;
+    buffer = new uint16_t[bufferSize];
+}
+
+TextBuffer::~TextBuffer()
+{
+    delete[] buffer;
+}
+
+void TextBuffer::setBuffer(uint16_t value)
+{
+    int counter;
+    unsigned *destination = (unsigned *)(buffer);
+    unsigned temp = value;
+    temp |= (temp << 16);
+
+    for (counter = bufferSize; counter > 1; counter -= 2)
+        *destination++ = temp;
+
+    if (counter)
+    {
+        buffer[bufferSize - 1] = value;
+    }
+}
+
+void TextBuffer::setBufferXY(int x, int y, uint16_t value)
+{
+    int offset = y * xres + x;
+
+    if (offset < bufferSize)
+        buffer[offset] = value;
+}
+
+void TextBuffer::setBufferXY(int x, int y, uint16_t *value, int len)
+{
+    int offset = y * xres + x;
+
+    while (len && (offset < bufferSize))
+    {
+        --len;
+        buffer[offset++] = *value++;
+    }
+}
+
+uint16_t TextBuffer::getBufferXY(int x, int y)
+{
+    int offset = y * xres + x;
+
+    if (offset < bufferSize)
+        return (buffer[offset]);
+    return 0;
+}
---
+++ textbuffer.h
@@ -0,0 +1,48 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TEXT_BUFFER_H_
+#define _TEXT_BUFFER_H_
+
+#include <cstdint>
+
+class TextBuffer
+{
+public:
+  TextBuffer(int xres, int yres);
+  ~TextBuffer();
+
+  uint16_t *getBuffer(void) const { return buffer; }
+
+  void setBuffer(uint16_t value);
+
+  void setBufferXY(int x, int y, uint16_t value);
+  void setBufferXY(int x, int y, uint16_t *value, int len);
+
+  uint16_t getBufferXY(int x, int y);
+
+  virtual void blitBuffer(int srcx, int srcy, int dstx, int dsty, char *dest){};
+
+private:
+  uint16_t *buffer;
+  int bufferSize;
+  int xres, yres;
+};
+
+#endif
