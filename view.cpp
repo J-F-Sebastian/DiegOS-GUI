1f4341f5163aa3aa9162039c3c2a9163f80e4174
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2022-01-15 14:59:47 +0100

    view: new class

---
+++ view.cpp
@@ -0,0 +1,632 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+
+#include "view.h"
+
+View::View(Rectangle &limits, ViewRender *rnd, Palette *pal, View *parent) : parentView(parent), renderer(rnd), palette(pal), limits(limits), rflags(0), sflags(VIEW_STATE_VISIBLE), oflags(0)
+{
+}
+
+View::~View()
+{
+	parentView = nullptr;
+	renderer = nullptr;
+	palette = nullptr;
+}
+
+void View::sizeLimits(Point &min, Point &max)
+{
+	min.x = min.y = 0;
+
+	if (parentView)
+	{
+		max.x = parentView->limits.width();
+		max.y = parentView->limits.height();
+	}
+	else
+	{
+		max.x = limits.width();
+		max.y = limits.height();
+	}
+}
+
+bool View::setLocation(const Rectangle &loc)
+{
+	Point min, max;
+	sizeLimits(min, max);
+	if ((loc.width() > max.x) ||
+	    (loc.width() < min.x) ||
+	    (loc.height() > max.y) ||
+	    (loc.height() < min.y))
+		return false;
+
+	changeLimits(loc);
+	return true;
+}
+
+bool View::moveLocation(const Point &delta)
+{
+	Rectangle temp(limits);
+	temp.move(delta.x, delta.y);
+
+	if (parentView)
+	{
+		if (!parentView->limits.includes(temp))
+		{
+			std::cout << "ERR" << std::endl;
+			return false;
+		}
+	}
+
+	changeLimits(temp);
+	return true;
+}
+
+void View::getOrigin(Point &origin) const
+{
+	origin = limits.ul;
+}
+
+void View::getExtent(Rectangle &extent)
+{
+	extent.ul = Point(0, 0);
+	extent.lr = Point(limits.width() - 1, limits.height() - 1);
+}
+
+void View::localize(Rectangle &rect)
+{
+	Point p = rect.ul;
+
+	/* Make coordinates local, relative to this view */
+	makeLocal(p);
+	rect.lr -= rect.ul;
+	rect.ul = p;
+	rect.lr += p;
+}
+
+void View::globalize(Rectangle &rect)
+{
+	Point p = rect.ul;
+
+	/* Make coordinates global, relative to the root view */
+	makeGlobal(p);
+	rect.lr -= rect.ul;
+	rect.ul = p;
+	rect.lr += p;
+}
+
+void View::setParent(View *par)
+{
+	if (!parentView && par)
+	{
+		parentView = par;
+	}
+}
+
+void View::setRenderer(ViewRender *rnd)
+{
+	if (!renderer && rnd)
+	{
+		renderer = rnd;
+	}
+}
+
+void View::setPalette(Palette *pal)
+{
+	if (pal && !palette)
+	{
+		palette = pal;
+	}
+}
+
+static const unsigned char RVALIDATE = VIEW_RESIZEABLE;
+
+void View::setResizeMode(unsigned char flags)
+{
+	if (flags & RVALIDATE)
+		rflags |= flags;
+}
+
+bool View::getResizeMode(unsigned char flags) const
+{
+	if (rflags & flags)
+		return true;
+	else
+		return false;
+}
+
+void View::clearResizeMode(unsigned char flags)
+{
+	if (rflags & RVALIDATE)
+		rflags &= ~flags;
+}
+
+void View::setOptions(unsigned char flags)
+{
+	if (flags & VIEW_OPT_ALL)
+		oflags |= flags;
+}
+
+bool View::getOptions(unsigned char flags) const
+{
+	if (oflags & flags)
+		return true;
+	else
+		return false;
+}
+
+void View::clearOptions(unsigned char flags)
+{
+	if (flags & VIEW_OPT_ALL)
+		oflags &= ~flags;
+}
+
+static const unsigned char SVALIDATE = (VIEW_STATE_VISIBLE |
+					VIEW_STATE_SELECTED |
+					VIEW_STATE_FOCUSED |
+					VIEW_STATE_DISABLED |
+					VIEW_STATE_DRAGGING |
+					VIEW_STATE_EVLOOP);
+
+void View::setState(unsigned char flags)
+{
+	if (flags & SVALIDATE)
+		sflags |= flags;
+}
+
+bool View::getState(unsigned char flags) const
+{
+	if (sflags & flags)
+		return true;
+	else
+		return false;
+}
+
+void View::clearState(unsigned char flags)
+{
+	if (flags & SVALIDATE)
+		sflags &= ~flags;
+}
+
+void View::handleEvent(Event *evt)
+{
+	if (isEventPositionValid(evt))
+	{
+		MouseEvent *mouse = evt->getMouseEvent();
+		if (mouse->pressed)
+		{
+			if (!getState(VIEW_STATE_SELECTED | VIEW_STATE_DISABLED) && getOptions(VIEW_OPT_SELECTABLE))
+			{
+				select();
+			}
+		}
+	}
+}
+
+bool View::isEventPositionValid(Event *evt)
+{
+	Rectangle lims;
+
+	if (!evt)
+		return false;
+
+	/* If the event is not positional return false */
+	if (!evt->isEventPositional())
+		return false;
+
+	MouseEvent *mouse = evt->getMouseEvent();
+	Point where(mouse->x, mouse->y);
+	getExtent(lims);
+	globalize(lims);
+	/* if it falls outside our limits then return false */
+
+	return lims.includes(where);
+}
+
+static const unsigned char FOCUSVAL = (VIEW_STATE_SELECTED | VIEW_STATE_EVLOOP);
+
+bool View::focus()
+{
+	if ((oflags & VIEW_OPT_SELECTABLE) == 0)
+		return false;
+
+	bool checkflags = !getState(VIEW_STATE_SELECTED | VIEW_STATE_EVLOOP);
+	if ((parentView == nullptr) || (checkflags && parentView->focus()))
+	{
+		select();
+		sflags |= VIEW_STATE_FOCUSED;
+		return true;
+	}
+
+	return false;
+}
+
+void View::select()
+{
+	Event evt;
+	MessageEvent cmd;
+
+	if (!getOptions(VIEW_OPT_SELECTABLE))
+		return;
+
+	setState(VIEW_STATE_SELECTED);
+	if (parentView)
+	{
+		if (parentView->getState(VIEW_STATE_FOCUSED))
+			setState(VIEW_STATE_FOCUSED);
+
+		cmd.senderObject = this;
+		cmd.destObject = parentView;
+		cmd.targetObject = this;
+		if (getOptions(VIEW_OPT_TOPSELECT))
+		{
+			cmd.command = CMD_FOREGROUND;
+		}
+		else
+		{
+			cmd.command = CMD_SELECT;
+		}
+		evt.setMessageEvent(cmd);
+		parentView->handleEvent(&evt);
+	}
+}
+
+void View::setLimits(const Rectangle &newrect)
+{
+
+	limits = newrect;
+}
+
+void View::getLimits(Rectangle &rect)
+{
+	rect = limits;
+}
+
+void View::calcLimits(const Point &delta, Rectangle &newrect)
+{
+	newrect = limits;
+	if (rflags & VIEW_RESIZE_UX)
+		newrect.ul.x += delta.x;
+	if (rflags & VIEW_RESIZE_UY)
+		newrect.ul.y += delta.y;
+	if (rflags & VIEW_RESIZE_LX)
+		newrect.lr.x += delta.x;
+	if (rflags & VIEW_RESIZE_LY)
+		newrect.lr.y += delta.y;
+}
+
+void View::changeLimits(const Rectangle &newrect)
+{
+	setLimits(newrect);
+	draw();
+}
+
+void View::makeLocal(Point &origin)
+{
+	if (parentView)
+	{
+		parentView->makeLocal(origin);
+	}
+	origin -= limits.ul;
+}
+
+void View::makeGlobal(Point &origin)
+{
+	if (parentView)
+	{
+		parentView->makeGlobal(origin);
+	}
+	origin += limits.ul;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+ViewGroup::ViewGroup(Rectangle &limits, ViewRender *rnd, PaletteGroup *pals, View *parent) : View(limits, rnd, nullptr, parent), lastLimits(limits), focused(nullptr), selected(nullptr), palettes(pals), viewList()
+{
+	setOptions(VIEW_OPT_SELECTABLE);
+}
+
+ViewGroup::~ViewGroup()
+{
+	List<View *>::iterator it = viewList.begin();
+	while (it != viewList.end())
+	{
+		delete (*it);
+		it++;
+	}
+	viewList.clear();
+}
+
+bool ViewGroup::setLocation(const Rectangle &loc)
+{
+	Rectangle update;
+	Point delta(0, 0);
+	Point min, max;
+
+	sizeLimits(min, max);
+	if ((loc.width() > max.x) ||
+	    (loc.width() < min.x) ||
+	    (loc.height() > max.y) ||
+	    (loc.height() < min.y))
+		return false;
+
+	getLimits(update);
+	delta.x = loc.width() - update.width();
+	delta.y = loc.height() - update.height();
+	setLimits(loc);
+
+	// No parenting means this is the root object, the main container of all graphics
+	if (!parentView)
+		renderer->clear(0);
+
+	List<View *>::iterator it = viewList.begin();
+	while (it != viewList.end())
+	{
+		(*it)->calcLimits(delta, update);
+		(*it)->setLocation(update);
+		it++;
+	}
+
+	// No parenting means this is the root object, the main container of all graphics
+	if (!parentView)
+		renderer->show();
+
+	return true;
+}
+
+bool ViewGroup::moveLocation(const Point &delta)
+{
+	if (View::moveLocation(delta))
+	{
+		return true;
+	}
+	return false;
+}
+
+void ViewGroup::draw()
+{
+	// No parenting means this is the root object, the main container of all graphics
+	if (!parentView)
+		renderer->clear(0);
+
+	// Draw children back-to-top, following the painter algorithm
+	for (List<View *>::riterator it = viewList.rbegin(); it != viewList.rend(); it++)
+	{
+		(*it)->draw();
+	}
+	// No parenting means this is the root object, the main container of all graphics
+	if (!parentView)
+		renderer->show();
+}
+
+void ViewGroup::handleEvent(Event *evt)
+{
+	View::handleEvent(evt);
+
+	/* If the event is positional, look for a view including the event in its
+	 * boundaries; first view accepting the coordinates of the event will process the
+	 * event with handleEvent() method.
+	 */
+	if (evt->isEventPositional())
+	{
+		List<View *>::iterator it;
+		for (it = viewList.begin(); it != viewList.end(); it++)
+		{
+			if ((*it)->isEventPositionValid(evt))
+				break;
+		}
+
+		if (it != viewList.end())
+			(*it)->handleEvent(evt);
+	}
+	/*
+	 * If the event is a message (a command) then the event is processed by either the
+	 * target view (if specified) or all views (if the target is the broadcast address).
+	 */
+	else if (evt->getEventType() == Event::EVT_CMD)
+	{
+		MessageEvent *msg = evt->getMessageEvent();
+		if ((msg->destObject == this) || (msg->destObject == BROADCAST_OBJECT))
+		{
+			switch (msg->command)
+			{
+			case CMD_FOREGROUND:
+				std::cout << "Foreground" << std::endl;
+				toTheTop(static_cast<View *>(msg->targetObject));
+				break;
+			case CMD_SELECT:
+				std::cout << "Select" << std::endl;
+				selectView(static_cast<View *>(msg->targetObject));
+				if (selected->getOptions(VIEW_OPT_TOPSELECT))
+					toTheTop(selected);
+				break;
+			}
+			if (msg->destObject == this)
+				return;
+		}
+
+		for (List<View *>::iterator it = viewList.begin(); it != viewList.end(); it++)
+		{
+			if ((msg->destObject == (*it)) || (msg->destObject == BROADCAST_OBJECT))
+			{
+				(*it)->handleEvent(evt);
+			}
+		}
+	}
+	/*
+	 * Other events are processed by all views.
+	 */
+	else
+	{
+		for (List<View *>::iterator it = viewList.begin(); it != viewList.end(); it++)
+			(*it)->handleEvent(evt);
+	}
+}
+
+void ViewGroup::insert(View *newView)
+{
+	if (newView)
+	{
+		newView->setParent(this);
+		newView->setRenderer(renderer);
+		if (newView->getOptions(VIEW_OPT_TOPSELECT))
+		{
+			viewList.addHead(newView);
+			if (selected)
+			{
+				selected->clearState(VIEW_STATE_SELECTED);
+			}
+			newView->setState(VIEW_STATE_SELECTED);
+			selected = newView;
+		}
+		else
+		{
+			viewList.addTail(newView);
+		}
+	}
+}
+
+void ViewGroup::setPalettes(PaletteGroup *pals)
+{
+	if (pals && !palettes)
+	{
+		palettes = pals;
+	}
+}
+
+bool ViewGroup::focusNext(bool forward)
+{
+	View *temp = nullptr;
+
+	if (viewList.count() < 1)
+		return false;
+
+	// FIXME missing check if focus can be handed off
+	if (forward)
+	{
+		List<View *>::iterator it(viewList, &focused);
+
+		if (++it == viewList.end())
+			it = viewList.begin();
+
+		temp = (*it);
+	}
+	else
+	{
+		List<View *>::riterator it(viewList, &focused);
+
+		if (++it == viewList.rend())
+			it = viewList.rbegin();
+
+		temp = (*it);
+	}
+
+	return focusView(temp);
+}
+
+void ViewGroup::selectNext(bool forward)
+{
+	View *temp = nullptr;
+
+	if (viewList.count() < 1)
+		return;
+
+	if (forward)
+	{
+		List<View *>::iterator it(viewList, &selected);
+
+		if (++it == viewList.end())
+			it = viewList.begin();
+
+		temp = (*it);
+	}
+	else
+	{
+		List<View *>::riterator it(viewList, &selected);
+
+		if (++it == viewList.rend())
+			it = viewList.rbegin();
+
+		temp = (*it);
+	}
+
+	selectView(temp);
+}
+
+bool ViewGroup::focusView(View *target)
+{
+	if (!target)
+		return false;
+
+	if (focused)
+	{
+		focused->clearState(VIEW_STATE_FOCUSED);
+	}
+	target->setState(VIEW_STATE_FOCUSED);
+	focused = target;
+
+	return true;
+}
+
+void ViewGroup::selectView(View *target)
+{
+	if (!target)
+		return;
+
+	if (selected)
+	{
+		selected->clearState(VIEW_STATE_SELECTED);
+	}
+	target->setState(VIEW_STATE_SELECTED);
+	selected = target;
+}
+
+void ViewGroup::toTheTop(View *target)
+{
+	if (viewList.count() < 1)
+		return;
+
+	List<View *>::iterator it(viewList, &target);
+
+	if (it != viewList.end())
+	{
+		it = viewList.erase(it);
+		std::cout << "count " << viewList.count() << std::endl;
+		viewList.addHead(target);
+		std::cout << "count " << viewList.count() << std::endl;
+	}
+}
+
+void ViewGroup::maximize()
+{
+	Rectangle max;
+	getLimits(max);
+	setLocation(max);
+}
+
+void ViewGroup::minimize()
+{
+	setLocation(lastLimits);
+}
---
+++ view.h
@@ -0,0 +1,372 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _VIEW_H_
+#define _VIEW_H_
+
+#include "list.h"
+#include "geometry.h"
+#include "event.h"
+#include "palettegroup.h"
+#include "viewrender.h"
+
+/*
+ * resize flags; there are 4 flags for resizing, one for each coordinate
+ * of a rectangle.
+ * Resizable means that the lower right point coordinates of a rectangle
+ * will be updated.
+ * The upper left point will remain unchanged, width and height will change.
+ * Bounded means that all coordinates will be updated.
+ * In the present implementation the "resize to" values are passed in with
+ * a Point class, so a Bounded view will not change its width and height,
+ * but only its position (as the same x and y values will be added to both
+ * upper left and lower right points).
+ */
+enum
+{
+	VIEW_RESIZE_UX = (1 << 0),
+	VIEW_RESIZE_UY = (1 << 1),
+	VIEW_RESIZE_LX = (1 << 2),
+	VIEW_RESIZE_LY = (1 << 3),
+	VIEW_RESIZEABLE = (VIEW_RESIZE_LX | VIEW_RESIZE_LY),
+	VIEW_BOUNDED = (VIEW_RESIZE_UX | VIEW_RESIZE_UY | VIEW_RESIZE_LX | VIEW_RESIZE_LY)
+};
+
+/*
+ * State flags
+ */
+enum
+{
+	/* View is visible */
+	VIEW_STATE_VISIBLE = (1 << 0),
+	/* View is selected, i.e. it is the actual selection */
+	VIEW_STATE_SELECTED = (1 << 1),
+	/* View has the focus, i.e. it will receive inputs from user */
+	VIEW_STATE_FOCUSED = (1 << 2),
+	/* View is disabled, no events are processed for this view */
+	VIEW_STATE_DISABLED = (1 << 3),
+	/* View is moving around or resizing */
+	VIEW_STATE_DRAGGING = (1 << 4),
+	/* View is running the event loop */
+	VIEW_STATE_EVLOOP = (1 << 5)
+};
+
+/*
+ * Option flags
+ */
+enum
+{
+	/* View can be selected or get the focus */
+	VIEW_OPT_SELECTABLE = (1 << 0),
+	/* View will be moved to foreground when selected */
+	VIEW_OPT_TOPSELECT = (1 << 1),
+	/* View will preprocess events */
+	VIEW_OPT_PREPROCESS = (1 << 2),
+	/* View will postprocess events */
+	VIEW_OPT_POSTPROCESS = (1 << 3),
+	/* View can be tiled inside its owner boundaries */
+	VIEW_OPT_TILEABLE = (1 << 4),
+	/* View can be centered w.r.t. owner's X axis */
+	VIEW_OPT_CENTERX = (1 << 5),
+	/* View can be centered w.r.t. owner's Y axis */
+	VIEW_OPT_CENTERY = (1 << 6),
+	/* View can be centered w.r.t. owner boundaries */
+	VIEW_OPT_CENTERED = (VIEW_OPT_CENTERX | VIEW_OPT_CENTERY),
+	/* View need validation */
+	VIEW_OPT_VALIDATE = (1 << 7),
+	VIEW_OPT_ALL = (VIEW_OPT_SELECTABLE |
+			VIEW_OPT_TOPSELECT |
+			VIEW_OPT_PREPROCESS |
+			VIEW_OPT_POSTPROCESS |
+			VIEW_OPT_TILEABLE |
+			VIEW_OPT_CENTERED |
+			VIEW_OPT_VALIDATE)
+};
+
+/*
+ * Class View is the basic interface to a graphical - or text view.
+ * Coordinates are referred to the owner's origin.
+ */
+class View
+{
+public:
+	virtual ~View();
+
+	/*
+	 * Compute new limits using resize flags.
+	 * Increments stored in delta are added to the view's limits
+	 * according to flags stored in rflags, the resulting rectangle
+	 * is stored in newrect.
+	 * 
+	 * PARAMETERS IN
+	 * const Point &delta - delta values
+	 * 
+	 * PARAMETERS OUT
+	 * Rectangle &newrect - the resulting rectangle after computation
+	 */
+	void calcLimits(const Point &delta, Rectangle &newrect);
+
+	/*
+	 * Compute minimum and maximum width and height and store
+	 * the results in min and max.
+	 * The default computation returns (0,0) in min and
+	 * (width, height) in max if owner is nil; if owner is valid
+	 * the width and height are those of the owner.
+	 * 
+	 * PARAMETERS OUT
+	 * Point &min - minimum width and height
+	 * Point &max - maximum width and height
+	 */
+	virtual void sizeLimits(Point &min, Point &max);
+
+	/*
+	 * Validate the new location coordinates stored in loc
+	 * and then invokes changeLimits to apply them.
+	 * The new location must be included in the owner limits
+	 * if owner is valid.
+	 * 
+	 * PARAMETERS IN
+	 * const Rectangle &loc - the new location
+	 * 
+	 * RETURN
+	 * true if the new location was validated
+	 * false if the new location was rejected
+	 */
+	virtual bool setLocation(const Rectangle &loc);
+
+	/*
+	 * Compute the new location coordinates adding
+	 * delta to the limits rectangle, validates the new location
+	 * and then invokes changeLimits to apply it.
+	 * The new location must be included in the owner limits
+	 * if owner is valid.
+	 * 
+	 * PARAMETERS IN
+	 * const Point &delta - delta coordinates to be added to the origin
+	 * 
+	 * RETURN
+	 * true if the new location was validated
+	 * false if the new location was rejected
+	 */
+	virtual bool moveLocation(const Point &delta);
+
+	/*
+	 * Copy the upper left point of limits into origin.
+	 * 
+	 * PARAMETERS OUT
+	 * Point &origin - view's origin
+	 */
+	void getOrigin(Point &origin) const;
+
+	void getExtent(Rectangle &extent);
+	void localize(Rectangle &rect);
+	void globalize(Rectangle &rect);
+
+	void setParent(View *par);
+	void setRenderer(ViewRender *rnd);
+	void setPalette(Palette *pal);
+
+	void setResizeMode(unsigned char flags);
+	bool getResizeMode(unsigned char flags) const;
+	void clearResizeMode(unsigned char flags);
+	void setOptions(unsigned char flags);
+	bool getOptions(unsigned char flags) const;
+	void clearOptions(unsigned char flags);
+	void setState(unsigned char flags);
+	bool getState(unsigned char flags) const;
+	void clearState(unsigned char flags);
+
+	virtual void draw(void) = 0;
+	virtual void handleEvent(Event *evt);
+
+	/*
+	 * Verify the validity of the event for this view and returns 
+	 * a boolean according to the evaluation.
+	 *
+	 * PARAMETERS IN
+	 * Event *evt - the event to be validated
+	 * 
+	 * RETURN
+	 * true if the event is valid for this view, i.e. the event has coordinates
+	 *      falling inside the view limits, and the event type is positional (mouse)
+	 * false if the event is not valid for this view and would not be processed by
+	 *       handleEvent
+	 */
+	virtual bool isEventPositionValid(Event *evt);
+
+protected:
+	explicit View(Rectangle &limits, ViewRender *rnd = nullptr, Palette *pal = nullptr, View *parent = nullptr);
+
+	/*
+	 * Apply new limits.
+	 * Limits are stored in a rectangle in owners coordinates.
+	 * 
+	 * PARAMETERS IN
+	 * const Rectangle &newrect - new view limits
+	 */
+	void setLimits(const Rectangle &newrect);
+
+	/*
+	 * Copy limits into rect.
+	 * Limits are stored in a rectangle in owners coordinates.
+	 * 
+	 * PARAMETERS OUT
+	 * Rectangle &rect - view limits
+	 */
+	void getLimits(Rectangle &rect);
+
+	/*
+	 * Apply new limits and draw the view.
+	 * Limits are stored in a rectangle in owners coordinates.
+	 * 
+	 * PARAMETERS IN
+	 * const Rectangle &newrect - the new boundaries
+	 */
+	void changeLimits(const Rectangle &newrect);
+
+	/*
+	 * General explanation: each view has its origin at (0,0) and expresses
+	 * coordinates inside its extents, or in other words, points inside a view
+	 * can have values ranging from (0,0) to (viewwidth - 1, viewheight - 1).
+	 * Now the same coordinates can be referred to the root view, which is supposed to be
+	 * the screen or a containing window.
+	 * The child view origin will have a pixel coordinates of (x, y) on screen, so
+	 * using global coordinates (screen coordinates) a point inside the child view
+	 * can have values ranging from (x, y) to (x + viewwidth - 1, y + viewheigth - 1).
+	 * 
+	 *origin+----------------------------------------+
+	 *     |                                        |
+	 *     |     (x,y)                              |
+	 *     |     origin+---------+                  |
+	 *     |           |(0,0)    |                  |
+	 *     |           |         |                  |
+	 *     |           +---------+                  |
+	 *     |                                        |
+	 *     |                                        |
+	 *     +----------------------------------------+
+	 */
+
+	/*
+	 * Converts coordinates from global (root parent)
+	 * to local (referred to the origin of the calling view).
+	 * origin is assumed to be valorized with root parent coordinates,
+	 * so that input origin(0,0) is the origin of the root parent, which
+	 * is usually the size of the screen.
+	 * 
+	 * PARAMETERS IN
+	 * Point &origin - global coordinates (screen coordinates)
+	 * 
+	 * PARAMETERS OUT
+	* Point &origin - local coordinates (view coordinates)
+	 */
+	void makeLocal(Point &origin);
+
+	/*
+	 * Converts coordinates from local (referred to the origin of the calling view)
+	 * to global (screen coordinates).
+	 * origin is assumed to be valorized with view coordinates,
+	 * so that input origin(0,0) is the upper left view corner.
+	 * 
+	 * PARAMETERS IN
+	 * Point &origin - local coordinates (view coordinates)
+	 * 
+	 * PARAMETERS OUT
+	* Point &origin - global coordinates (screen coordinates)
+	 */
+	void makeGlobal(Point &origin);
+
+	View *getParent(void) { return parentView; }
+	ViewRender *getRenderer(void) { return renderer; }
+
+	virtual bool focus(void);
+	virtual void select(void);
+
+	View *parentView;
+	ViewRender *renderer;
+	Palette *palette;
+
+private:
+	/*
+	 * Limits is expressed in LOCAL coordinates
+	 */
+	Rectangle limits;
+	unsigned char rflags, sflags, oflags;
+};
+
+class ViewGroup : public View
+{
+public:
+	explicit ViewGroup(Rectangle &limits, ViewRender *rnd, PaletteGroup *pals, View *parent = nullptr);
+	virtual ~ViewGroup();
+
+	/*
+	 * Validate the new location coordinates stored in loc
+	 * and then invokes changeLimits to apply them.
+	 * The new location must be included in the owner limits
+	 * if owner is valid.
+	 * 
+	 * PARAMETERS IN
+	 * const Rectangle &loc - the new location
+	 */
+	virtual bool setLocation(const Rectangle &loc) override;
+	/*
+	 * Compute the new location coordinates adding
+	 * delta to the limits rectangle, validates the new location
+	 * and then invokes changeLimits to apply it.
+	 * The new location must be included in the owner limits
+	 * if owner is valid.
+	 * 
+	 * PARAMETERS IN
+	 * const Point &delta - delta coordinates to be added to the origin
+	 * 
+	 * RETURN
+	 * true if the new location was validated
+	 * false if the new location was rejected
+	 */
+	virtual bool moveLocation(const Point &delta) override;
+
+	virtual void draw(void) override;
+
+	virtual void handleEvent(Event *evt) override;
+
+	void maximize(void);
+	void minimize(void);
+
+	void insert(View *newView);
+
+	void setPalettes(PaletteGroup *pals);
+
+protected:
+	bool focusNext(bool forward);
+	void selectNext(bool forward);
+
+	bool focusView(View *target);
+	void selectView(View *target);
+	void toTheTop(View *target);
+
+	Rectangle lastLimits;
+	View *focused;
+	View *selected;
+	PaletteGroup *palettes;
+
+private:
+	List<View *> viewList;
+};
+
+#endif
