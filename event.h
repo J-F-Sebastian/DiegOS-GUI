443a345585c4bae5892db4d01e2daf8849e6562a
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2022-12-11 18:28:18 +0100

    event.h: indentation

--- event.h
+++ event.h
@@ -24,48 +24,48 @@
 
 struct KeyDownEvent
 {
-    uint16_t keyCode;
-    uint16_t modifier;
+	uint16_t keyCode;
+	uint16_t modifier;
 };
 
 enum
 {
-    POS_EVT_RIGHT = 1 << 0,
-    POS_EVT_MIDDLE = 1 << 1,
-    POS_EVT_LEFT = 1 << 2,
-    POS_EVT_TAP = 1 << 3,
+	POS_EVT_RIGHT = 1 << 0,
+	POS_EVT_MIDDLE = 1 << 1,
+	POS_EVT_LEFT = 1 << 2,
+	POS_EVT_TAP = 1 << 3,
 };
 
 enum
 {
-    // Single click
-    POS_EVT_PRESSED = 1 << 0,
-    // Double click
-    POS_EVT_DOUBLE = 1 << 1,
-    // Button pressed while moving
-    POS_EVT_LONG = 1 << 2
+	// Single click
+	POS_EVT_PRESSED = 1 << 0,
+	// Double click
+	POS_EVT_DOUBLE = 1 << 1,
+	// Button pressed while moving
+	POS_EVT_LONG = 1 << 2
 };
 
 struct PositionalEvent
 {
-    int x, y;
-    /*
-     * Right --------
-     * Middle ------ \
-     * Left ------- \ \
-     * Tap ------- \ \ \
-     *            \ \ \ \
-     *    7 6 5 4 3 2 1 0
-     */
-    uint8_t buttons;
-    /*
-     * Pressed ---------------
-     * Double Pressure ------ \
-     * Long Pressure ------- \ \
-     *                      \ \ \
-     *            7 6 5 4 3 2 1 0
-     */
-    uint8_t status;
+	int x, y;
+	/*
+	 * Right --------
+	 * Middle ------ \
+	 * Left ------- \ \
+	 * Tap ------- \ \ \
+	 *            \ \ \ \
+	 *    7 6 5 4 3 2 1 0
+	 */
+	uint8_t buttons;
+	/*
+	 * Pressed ---------------
+	 * Double Pressure ------ \
+	 * Long Pressure ------- \ \
+	 *                      \ \ \
+	 *            7 6 5 4 3 2 1 0
+	 */
+	uint8_t status;
 };
 
 /*
@@ -73,34 +73,34 @@ struct PositionalEvent
  */
 enum
 {
-    /* Quit the application */
-    CMD_QUIT,
-    /* View will be closed and removed */
-    CMD_CLOSE,
-    /* View will be zoomed, i.e. will resize to the extents of the parent */
-    CMD_MAXIMIZE,
-    /* View will be resized to the minimum extent */
-    CMD_MINIMIZE,
-    /* View will return to the original size */
-    CMD_RESTORE,
-    /* A view is signalling an input event, data are ready to be handled */
-    CMD_INPUT,
-    /* A view is being moved around */
-    CMD_DRAGGING,
-    /* A view is being resized */
-    CMD_RESIZING,
-    /* Move the view to the top of the list */
-    CMD_FOREGROUND,
-    /* Select a view */
-    CMD_SELECT,
-    /* Focus a view */
-    CMD_FOCUS,
-    /* Update the view with data found in the message */
-    CMD_UPDATE,
-    /* Redraw the view */
-    CMD_REDRAW,
-    /* Draw all views */
-    CMD_DRAW
+	/* Quit the application */
+	CMD_QUIT,
+	/* View will be closed and removed */
+	CMD_CLOSE,
+	/* View will be zoomed, i.e. will resize to the extents of the parent */
+	CMD_MAXIMIZE,
+	/* View will be resized to the minimum extent */
+	CMD_MINIMIZE,
+	/* View will return to the original size */
+	CMD_RESTORE,
+	/* A view is signalling an input event, data are ready to be handled */
+	CMD_INPUT,
+	/* A view is being moved around */
+	CMD_DRAGGING,
+	/* A view is being resized */
+	CMD_RESIZING,
+	/* Move the view to the top of the list */
+	CMD_FOREGROUND,
+	/* Select a view */
+	CMD_SELECT,
+	/* Focus a view */
+	CMD_FOCUS,
+	/* Update the view with data found in the message */
+	CMD_UPDATE,
+	/* Redraw the view */
+	CMD_REDRAW,
+	/* Draw all views */
+	CMD_DRAW
 };
 
 /*
@@ -108,95 +108,95 @@ enum
  */
 enum
 {
-    /* OK */
-    RESPONSE_OK,
-    /* Cancel the operation */
-    RESPONSE_CANCEL,
-    /* Apply data or action */
-    RESPONSE_APPLY,
-    /* Move to the previous item (can be anything) */
-    RESPONSE_PREV,
-    /* Move to the next item (can be anything) */
-    RESPONSE_NEXT
+	/* OK */
+	RESPONSE_OK,
+	/* Cancel the operation */
+	RESPONSE_CANCEL,
+	/* Apply data or action */
+	RESPONSE_APPLY,
+	/* Move to the previous item (can be anything) */
+	RESPONSE_PREV,
+	/* Move to the next item (can be anything) */
+	RESPONSE_NEXT
 };
 
 #define BROADCAST_OBJECT (void *)(-1)
 struct MessageEvent
 {
-    /* Command ID */
-    uint16_t command;
-    /* Sub command ID */
-    uint16_t subCommand;
-    /*
-     * The object sending this event, i.e. the originator.
-     * This value must be assigned anytime to a valid value.
-     * It cannot be set to BROADCAST_OBJECT.
-     */
-    void *senderObject;
-    /*
-     * The object receiving this event, i.e. the destination.
-     * It can be set to BROADCAST_OBJECT to send the event to all objects
-     * in a group.
-     * The destination is supposed to be the object that process the message.
-     */
-    void *destObject;
-    /*
-     * The target of the command, i.e. the object the action is to be applied upon.
-     * destObject is supposed to process the command and apply it to targetObject.
-     */
-    void *targetObject;
-    /*
-     * Optional payload, up to the message implementation.
-     */
-    void *payload;
+	/* Command ID */
+	uint16_t command;
+	/* Sub command ID */
+	uint16_t subCommand;
+	/*
+	 * The object sending this event, i.e. the originator.
+	 * This value must be assigned anytime to a valid value.
+	 * It cannot be set to BROADCAST_OBJECT.
+	 */
+	void *senderObject;
+	/*
+	 * The object receiving this event, i.e. the destination.
+	 * It can be set to BROADCAST_OBJECT to send the event to all objects
+	 * in a group.
+	 * The destination is supposed to be the object that process the message.
+	 */
+	void *destObject;
+	/*
+	 * The target of the command, i.e. the object the action is to be applied upon.
+	 * destObject is supposed to process the command and apply it to targetObject.
+	 */
+	void *targetObject;
+	/*
+	 * Optional payload, up to the message implementation.
+	 */
+	void *payload;
 };
 
 class Event
 {
 public:
-    enum EventType
-    {
-        EVT_UNKNOWN,
-        EVT_POS,
-        EVT_KBD,
-        EVT_CMD
-    };
+	enum EventType
+	{
+		EVT_UNKNOWN,
+		EVT_POS,
+		EVT_KBD,
+		EVT_CMD
+	};
 
-    Event() { myEventData.what = EVT_UNKNOWN; }
-    explicit Event(const Event &other) { myEventData = other.myEventData; }
-    explicit Event(const PositionalEvent &pos);
-    explicit Event(const KeyDownEvent &kbd);
-    explicit Event(const MessageEvent &cmd);
+	Event() { myEventData.what = EVT_UNKNOWN; }
+	explicit Event(const Event &other) { myEventData = other.myEventData; }
+	explicit Event(const PositionalEvent &pos);
+	explicit Event(const KeyDownEvent &kbd);
+	explicit Event(const MessageEvent &cmd);
 
-    enum EventType getEventType(void);
-    bool isEventPositional(void);
-    bool isEventKey(void);
-    bool isEventUnknown(void);
+	enum EventType getEventType(void);
+	bool isEventPositional(void);
+	bool isEventKey(void);
+	bool isEventUnknown(void);
 
-    struct PositionalEvent *getPositionalEvent(void);
-    struct KeyDownEvent *getKeyDownEvent(void);
-    struct MessageEvent *getMessageEvent(void);
+	struct PositionalEvent *getPositionalEvent(void);
+	struct KeyDownEvent *getKeyDownEvent(void);
+	struct MessageEvent *getMessageEvent(void);
 
-    void setPositionalEvent(const PositionalEvent &pos);
-    void setKeyDownEvent(const KeyDownEvent &kbd);
-    void setMessageEvent(const MessageEvent &cmd);
+	void setPositionalEvent(const PositionalEvent &pos);
+	void setKeyDownEvent(const KeyDownEvent &kbd);
+	void setMessageEvent(const MessageEvent &cmd);
 
-    bool testPositionalEventPos(char bitmap);
-    bool testPositionalEventStatus(char bitmap);
+	bool testPositionalEventPos(char bitmap);
+	bool testPositionalEventStatus(char bitmap);
 
-    void clear(void);
+	void clear(void);
 
 private:
-    struct EventData
-    {
-        EventType what;
-        union
-        {
-            PositionalEvent position;
-            KeyDownEvent keyDown;
-            MessageEvent message;
-        };
-    } myEventData;
+	struct EventData
+	{
+		EventType what;
+		union
+		{
+			PositionalEvent position;
+			KeyDownEvent keyDown;
+			MessageEvent message;
+		};
+	} myEventData;
 };
 
 #endif
