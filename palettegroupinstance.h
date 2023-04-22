c0f83c6c75fcb9d439e9324af62992925413c2f3
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2023-04-22 15:09:30 +0200

    palettegroupinstance: new class

---
+++ palettegroupinstance.h
@@ -0,0 +1,50 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2019 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PALETTEGROUPINSTANCE_H_
+#define _PALETTEGROUPINSTANCE_H_
+
+#include "palettegroupfactory.h"
+
+class PaletteGroupInstance
+{
+public:
+	/*
+	 * Configure the system palette with the specified type and bit depth.
+	 * This method can be invoked runtime at will to change the colors theme.
+	 */
+	void configure(enum PaletteGroupType sel, int bitdepth);
+
+	/*
+	 * Retrieve the configured PaletteGroup object.
+	 */
+	class PaletteGroup *get();
+
+	/*
+	 * Retrieve the singleton instance.
+	 */
+	static PaletteGroupInstance *instance();
+
+private:
+	class PaletteGroup *pg;
+
+	PaletteGroupInstance();
+};
+
+#endif
