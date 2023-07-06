8e7b1d31732d6c015d50cdf0a1727876fd52bc57
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2023-07-07 00:00:04 +0200

    viewapplication.cpp: refactor handleEvent()

--- viewapplication.cpp
+++ viewapplication.cpp
@@ -2,6 +2,8 @@
 #include "desktop.h"
 #include "event_keyboard.h"
 
+#define VIEWLISTITFOR(x) for (List<View *>::iterator x = viewList.begin(); x != viewList.end(); x++)
+
 ViewApplication::ViewApplication(Rectangle &limits, ViewEventManager *evt, View *parent) : ViewExec(limits, evt, parent), background(nullptr)
 {
 }
@@ -27,7 +29,43 @@ void ViewApplication::handleEvent(Event *evt)
 {
 	// evt->print();
 
-	if (evt->isEventKey())
+	if (evt->isEventCommand())
+	{
+		MessageEvent *msg = evt->getMessageEvent();
+		/*
+		 * All objects in the tree
+		 */
+		if (isCommandAny(msg))
+		{
+			switch (msg->command)
+			{
+			case CMD_REDRAW:
+				std::cout << "REDRAW CMD all" << std::endl;
+				reDraw();
+				evt->clear();
+				break;
+
+			case CMD_DRAW:
+				std::cout << "DRAW CMD all" << std::endl;
+				draw();
+				evt->clear();
+				break;
+
+			case CMD_QUIT:
+				std::cout << "QUIT CMD all" << std::endl;
+				forEachExecuteCommand(msg);
+				VIEWLISTITFOR(it)
+				{
+					delete (*it);
+				}
+				viewList.clear();
+				actual = nullptr;
+				evt->clear();
+				break;
+			}
+		}
+	}
+	else if (evt->isEventKey())
 	{
 		KeybEvent *key = evt->getKeyDownEvent();
 		switch (key->keyCode)
@@ -36,11 +74,11 @@ void ViewApplication::handleEvent(Event *evt)
 			if (key->modifier & KBD_MOD_CTRL)
 			{
 				Event event;
-				sendCommand(CMD_QUIT, BROADCAST_OBJECT, BROADCAST_OBJECT);
+				sendCommand(CMD_QUIT);
 				/*
 				 * On exit dequeue all pending events before termination
 				 */
-				while (evtM->wait(&event, 0))
+				while (evtM->wait(&event, 100))
 					handleEvent(&event);
 				draw();
 				clearState(VIEW_STATE_EVLOOP);
