9f103ee455552992bb63de3513f6a8e0d7635ade
Diego Gallizioli <diego.gallizioli@libero.it>
2024-03-24 21:22:07 +0100

    viewapplication: add missing license, cleanup leftovers

--- viewapplication.cpp
+++ viewapplication.cpp
@@ -1,8 +1,27 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2022 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "viewapplication.h"
 #include "desktop.h"
 #include "event_keyboard.h"
 
-#define VIEWLISTITFOR(x) for (List<View *>::iterator x = viewList.begin(); x != viewList.end(); x++)
+#include <iostream>
 
 ViewApplication::ViewApplication(Rectangle &limits, ViewEventManager *evt, View *parent) : ViewExec(limits, evt, parent), background(nullptr)
 {
