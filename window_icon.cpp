c3c370d24c16cf38cdf5bd7428b6b40a60088fc0
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-04-28 16:36:30 +0200

    window_icon: refactor drawView() using frame()

--- window_icon.cpp
+++ window_icon.cpp
@@ -35,9 +35,9 @@ void WindowIconClose::drawView()
 	Palette *palette = GPaletteGroup->getPalette(PaletteGroup::PAL_WINICON);
 	getViewport(viewRect);
 
-	unsigned color, color2;
-	palette->getPalette(WINICON_BRIGHT, color);
-	palette->getPalette(WINICON_DARK, color2);
+	unsigned color[2];
+	palette->getPalette(WINICON_BRIGHT, color[0]);
+	palette->getPalette(WINICON_DARK, color[1]);
 
 	// Outer shadow
 
@@ -48,61 +48,57 @@ void WindowIconClose::drawView()
 	 *    B       D
 	 *    DDDDDDDDD
 	 */
-	Point ul(viewRect.ul);
-	renderer->hline(ul, viewRect.width(), color);
-	renderer->vline(ul, viewRect.height() - 1, color);
-	ul.move(viewRect.width(), 1);
-	renderer->vline(ul, viewRect.height() - 1, color2);
-	ul.move(-viewRect.width(), viewRect.height() - 1);
-	renderer->hline(ul, viewRect.width() - 1, color2);
+	renderer->frame(viewRect, color, false);
+	viewRect.zoom(-1, -1);
+	renderer->frame(viewRect, color, false);
 
 	// The button frame
-	palette->getPalette(WINICON_MAIN, color);
+	palette->getPalette(WINICON_MAIN, color[0]);
 	viewRect.zoom(-1, -1);
-	renderer->filledRectangle(viewRect, color);
+	renderer->filledRectangle(viewRect, color[0]);
 
 	if (isDown())
 	{
-		palette->getPalette(WINICON_PRESSED, color);
+		palette->getPalette(WINICON_PRESSED, color[0]);
 	}
 	else
 	{
-		palette->getPalette(WINICON_RELEASED, color);
+		palette->getPalette(WINICON_RELEASED, color[0]);
 	}
 
 	viewRect.zoom(-2, -2);
 	Point s(viewRect.ul), e(viewRect.lr);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(1, 0);
 	e.move(0, -1);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(1, 0);
 	e.move(0, -1);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(-2, 1);
 	e.move(-1, 2);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(0, 1);
 	e.move(-1, 0);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 
 	s.x = viewRect.lr.x;
 	s.y = viewRect.ul.y;
 	e.x = viewRect.ul.x;
 	e.y = viewRect.lr.y;
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(-1, 0);
 	e.move(0, -1);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(-1, 0);
 	e.move(0, -1);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(2, 1);
 	e.move(1, 2);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 	s.move(0, 1);
 	e.move(1, 0);
-	renderer->line(s, e, color);
+	renderer->line(s, e, color[0]);
 }
 
 void WindowIconClose::doAction()
@@ -182,8 +178,9 @@ void WindowIconZoom::drawView()
 
 void WindowIconZoom::doAction()
 {
+
 	if (getParent())
 	{
-		sendCommand(getParent()->getResizeMode(VIEW_ZOOMED) ? CMD_RESTORE : CMD_MAXIMIZE, getParent(), getParent());
+		sendCommand(/*getParent()->getResizeMode(VIEW_ZOOMED) ? CMD_RESTORE : CMD_MAXIMIZE*/ CMD_ZOOM, getParent(), getParent());
 	}
 }
