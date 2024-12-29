895b847fb1236b74226cea9f0723182c357d486c
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-12-29 20:01:01 +0100

    vieweventsdl.cpp: change SDL2 include path

--- vieweventsdl.cpp
+++ vieweventsdl.cpp
@@ -16,10 +16,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <iostream>
+#include <SDL2/SDL.h>
 
 #include "vieweventsdl.h"
-#include "SDL.h"
 
 /*
  * Static caches for events, this is not truly required for keyboard events,
